b3826c2e50bc34d31cd3eb17613f7716d44e839c
Ty Mazey <tymazey@gmail.com>
2019-03-27 10:29:32 -0600

    updates test and service for NREL service

--- app/services/nrel_service.rb
+++ app/services/nrel_service.rb
@@ -3,4 +3,33 @@ class NRELService
   def initialize(search_params)
     @search_params = search_params
   end
+
+
+  def get_stations
+    @stations ||= parse_response(get_response)
+  end
+
+  private
+
+  def conn
+    Faraday.new(url: "https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest") do |faraday|
+      faraday.adapter Faraday.default_adapter
+    end
+  end
+
+  def get_response(zip)
+    conn.get do |req|
+      req.params['status'] = 'E'
+      req.params['access'] = 'public'
+      req.params['fuel_type'] = 'ELEC,LPG'
+      req.params['zip'] = zip
+      req.params['limit'] = '15'
+      req.params['location'] = zip
+      req.params['api_key'] = ENV[NREL_API_KEY]
+    end
+  end
+
+    def parse_response(response)
+      JSON.parse(response)
+    end
 end
--- spec/services/nrel_service_spec.rb
+++ spec/services/nrel_service_spec.rb
@@ -11,11 +11,16 @@ describe 'nrel service' do
   describe 'instance methods' do
     describe '.get_stations' do
       it 'returns json for stations by zip code' do
-        result = NRELService.new("80206").get_stations
+          VCR.use_cassette("services/NRELService") do
+          result = NRELService.new("80206").get_stations
 
-        expected = {}
-        
-        expect(result).to eq(expected)
+          expected = {}
+
+          expect(result).to be_a(Hash)
+          expect(result).to have_key(:total_results)
+          expect(result).to have_key(:station_counts)
+          expect(result).to have_key(:fuel_stations)
+        end
       end
     end
   end
