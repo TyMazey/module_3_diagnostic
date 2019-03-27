a7955f4c02d0e41701bff9ca401e1f77e419850a
Ty Mazey <tymazey@gmail.com>
2019-03-27 09:54:29 -0600

    build out Search Facade to use a service(not made) and make Station models(unmade)

--- app/facades/search_facade.rb
+++ app/facades/search_facade.rb
@@ -1,7 +1,17 @@
 class SearchFacade
 
   def initialize(search_params)
+    @api_results = NRELService.new(search_params)
+  end
 
-    @search_params = search_params
+
+  def stations
+    @stations ||= @api_results.map do |station_info|
+      Station.new(station_info)
+    end
+  end
+
+  def result_count
+    @stations.count
   end
 end
