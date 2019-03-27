c15536f8d109ce4643477b560493dde56e3acb7e
Ty Mazey <tymazey@gmail.com>
2019-03-27 09:28:23 -0600

    adds SearchFacade to searchcontroller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,13 @@
 class SearchController < ApplicationController
 
   def index
+    render locals: {facade: SearchFacade.new(search_params)}
+  end
 
+
+  private
+
+  def search_params
+    params.permit(:q)
   end
 end
