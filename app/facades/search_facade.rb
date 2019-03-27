bbae8d4ca27769d2041f5c8c3c6251362f24f7ed
Ty Mazey <tymazey@gmail.com>
2019-03-27 10:47:01 -0600

    adds new VCR to facades

--- app/facades/search_facade.rb
+++ app/facades/search_facade.rb
@@ -1,17 +1,17 @@
 class SearchFacade
 
   def initialize(search_params)
-    @api_results = NRELService.new(search_params)
+    @api_results = NRELService.new(search_params).get_stations
   end
 
 
   def stations
-    @stations ||= @api_results.map do |station_info|
+    @stations ||= @api_results["fuel_stations"].map do |station_info|
       Station.new(station_info)
     end
   end
 
   def result_count
-    @stations.count
+    @api_results["total_results"]
   end
 end
--- spec/facades/search_facade_spec.rb
+++ spec/facades/search_facade_spec.rb
@@ -13,18 +13,22 @@ describe 'SearchFacade' do
   describe 'instance methods' do
     describe '.result_count' do
       it 'can return the total of a search result' do
-        facade = SearchFacade.new("80206")
+        VCR.use_cassette("facades/NRELService") do
+          facade = SearchFacade.new("80206").
 
-        expect(facade.result_count).to eq(15)
+          expect(facade.result_count).to eq(4)
+        end
       end
     end
 
     describe '.stations' do
       it 'returns a list of stations within 5 miles of a zip code' do
-        facade = SearchFacade.new("80206")
+        VCR.use_cassette("facades/NRELService") do
+          facade = SearchFacade.new("80206")
 
-        expect(facade.stations).to be_a(Array)
-        expect(facade.stations.first).to be_a(Station)
+          expect(facade.stations).to be_a(Array)
+          expect(facade.stations.first).to be_a(Station)
+        end
       end
     end
   end
